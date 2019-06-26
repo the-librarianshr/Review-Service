d77a48cf5aca1de450d3a76014b33dc39a2c022d
RomanEmmons <roman.emmons@gmail.com>
2019-06-26 10:50:03 -0700

    progress with server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3010;
+
+app.get("/reviews", (req, res) => {});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
