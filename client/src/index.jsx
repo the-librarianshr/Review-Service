5b24cd9136df5632825059889a1738ab64c17007
RomanEmmons <roman.emmons@gmail.com>
2019-06-26 10:51:44 -0700

    progress

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
-const App = require("./components/App.jsx");
+const App = require("./components/reviews.jsx");
 
 ReactDOM.render(<App />, document.getElementById("app"));
